3240c6636a042cd1ebb84797dfce2d1e8c09bd29
Razi D <racieldiz@gmail.com>
2020-06-30 16:38:57 -0700

    Updates

---
+++ precache-manifest.5b797d5a85af8bfacbc25d4f2a97930d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "561f28c1f7d45794afe02eea0e958e8a",
+    "url": "/MadDogFlash/Voting-system/index.html"
+  },
+  {
+    "revision": "fe54d9d8fe2e49b0fbc0",
+    "url": "/MadDogFlash/Voting-system/static/css/main.4289073e.chunk.css"
+  },
+  {
+    "revision": "eac0db702b77553fa215",
+    "url": "/MadDogFlash/Voting-system/static/js/2.e91896fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MadDogFlash/Voting-system/static/js/2.e91896fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe54d9d8fe2e49b0fbc0",
+    "url": "/MadDogFlash/Voting-system/static/js/main.24cbe713.chunk.js"
+  },
+  {
+    "revision": "6a6f0b6f748b2cd8431c",
+    "url": "/MadDogFlash/Voting-system/static/js/runtime-main.7ff64555.js"
+  }
+]);
